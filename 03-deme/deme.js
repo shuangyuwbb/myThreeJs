c80406bd36b5be06c59848ca8385d08a7727e9b0
unknown <wubinbin@ad.fscut.com>
2020-11-12 15:48:28 +0800

    'deme完成版'

---
+++ 03-deme/deme.js
@@ -0,0 +1,62 @@
+var camera
+var scene
+var renderer
+var shape
+function initObject(arr){
+    shape = new THREE.Shape();//shapes — 形状或者一个包含形状的数组。
+    // 遍历数组将所有二维坐标连接成面
+    for (let i=0; i<arr.length; i++){
+        if(i === 0){
+            shape.moveTo(arr[i].x, arr[i].y)
+        }else if(i === arr.length - 1){
+            shape.lineTo(arr[0].x, arr[0].y)
+        }else{
+            shape.lineTo(arr[i].x, arr[i].y)
+        }
+    }
+    // 拉伸相关配置
+    var extrudeSettings = {
+        steps: 1,
+        depth: 1, // 拉伸厚度
+        bevelEnabled: false,// 拉伸的图形是否为斜角
+    };
+    // 创建立体模型
+    var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); // 材质设置
+    var mesh = new THREE.Mesh( geometry, material ) ;
+    // scene.add( mesh );
+    return mesh
+}
+
+function onResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight)
+}
+
+function render(){
+    renderer.render(scene, camera);
+    requestAnimationFrame(render)
+}
+
+//窗口大小变化监听
+window.addEventListener('resize',onResize,false);
+
+function init(arr1, arr2){
+    scene = new THREE.Scene()
+    camera = initCamera()
+    let axios = new  THREE.AxesHelper(90);
+    new THREE.OrbitControls(camera)//创建控件对象
+    initDefaultLighting(scene)
+    scene.add(axios)
+    renderer = initRenderer()
+    let object1 = initObject(arr1)
+    let object2 = initObject(arr2)
+    subtract(object1, object2)
+
+    render()
+}
+
+
+
+
